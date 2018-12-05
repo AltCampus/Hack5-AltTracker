ef1416418b3c9dcb38c40fc6823428d9ab638571
ashwanig3 <goswami17829@gmail.com>
2018-12-05 16:51:18 +0530

    initialize server and add dependencies

--- server/app.js
+++ server/app.js
@@ -3,7 +3,9 @@ const app = express();
 
 const port = 4400;
 
-
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)

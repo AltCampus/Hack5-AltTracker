6a52e748974483346cbe9ff195ef412a505b5c93
ashwanig3 <goswami17829@gmail.com>
2018-12-05 17:39:22 +0530

    initialize mongoose

--- server/app.js
+++ server/app.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 
 const port = 4400;
+
+mongoose.connect('mongodb://localhost/altTracker',{ useNewUrlParser: true },  function(err, connection) {
+  if(err) throw err
+  else console.log('Connected to mongodb')
+});
+
+const event = new mongoose.Schema({
+        event_name: String,
+        teams: [
+            {
+                team_name: String,
+                teamMembers: [String],
+                team_task: String
+            }
+        ]  
+})
+const Event = mongoose.model('Event', event);
+
+
 
 app.get('/', (req, res) => {
     res.send('hello world')
